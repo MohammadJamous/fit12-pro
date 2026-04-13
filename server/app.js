851c1125381665bc64a3c6ba31d2fcb46a56ee8b
‪Mohammad Jamous‬‏ <118446184+MohammadJamous@users.noreply.github.com>
2026-04-13 23:23:51 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@ require("dotenv").config();
 
 require("./config/db");
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const authRoutes = require("./routes/authRoutes");
 const workoutRoutes = require("./routes/workoutRoutes");
 const dietRoutes = require("./routes/dietRoutes");
 const productRoutes = require("./routes/productRoutes");
 const progressRoutes = require("./routes/progressRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -34,8 +38,35 @@ app.use("/workouts", workoutRoutes);
 app.use("/diet", dietRoutes);
 app.use("/products", productRoutes);
 app.use("/progress", progressRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 8080;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://fit12-pro.netlify.app",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+let onlineUsers = 0;
+app.set("onlineUsers", onlineUsers);
+
+io.on("connection", (socket) => {
+  onlineUsers += 1;
+  app.set("onlineUsers", onlineUsers);
+  io.emit("onlineUsers", onlineUsers);
+
+  socket.on("disconnect", () => {
+    onlineUsers = Math.max(0, onlineUsers - 1);
+    app.set("onlineUsers", onlineUsers);
+    io.emit("onlineUsers", onlineUsers);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
